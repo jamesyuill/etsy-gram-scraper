ef4f2fd72dc8814a19b218fd4102a1f6fef177da
James Yuill <jamesyuill@jamess-mbp.lan>
2024-01-06 15:13:46 +0000

    updates with args and executable path

--- utils/getEtsyDetails.js
+++ utils/getEtsyDetails.js
@@ -2,6 +2,16 @@ import puppeteer from 'puppeteer';
 
 const getEtsyDetails = async () => {
   const browser = await puppeteer.launch({
+    args: [
+      '--no-sandbox',
+      '--disable-setuid-sandbox',
+      '--single-process',
+      '--no-zygote',
+    ],
+    executablePath:
+      process.env.NODE_ENV === 'production'
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
     headless: 'new',
   });
 
